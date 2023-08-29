63c6a2005cfd277b39f86566e7f8551307f3819e
Pankaj Jha <pankaj.j@copperdigital.com>
2023-08-29 18:52:52 +0530

    First push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+// import crypt from '../assets/CryptLib';
+import cryptlib from '../assets/CryptLib'
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demoAngular';
+  ngOnInit() {
+    console.log(cryptlib.decryptCipherTextWithRandomIV("TEi0/REmkb6AMKD6O+TjBz+AZeBsMVz8Ba+J20EY2YM=", "9psmJHiPf2F6LhT2oHH1ZS7D90baDGfnNNYl1m2QN2FYnsYKkplo9kfCgacAupnIFc11LAuOfQrc7d/e16J95mXx6WLXvd9qF+A8jc85fbXc5ObXvUs0NfDMiK6DRzmMnQrPzcqtFKqkGsbdyFC6ULKsu9PNeYaTITAgOGBd/Yu69NBOJjbilN7+6YcE6xsesdcEfKVnaZ1MbhJwvhT2LYB/hgw8CoQ5CqMUssbz9dTdxsYkHbqn/jiXSpcIRvDiJeN8jTFl5Js8fdyfOnSifIMp/yjGyHS6gc828+I2f7ZeIhNugokHtjuIpi1ehAcfcpOcixo/Ax39SxVOWB5/VA=="))
+  }
 }
